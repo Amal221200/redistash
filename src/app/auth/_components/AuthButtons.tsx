0cb9798805c3a148c8608d3d640d6cc6d47eb6f3
Amal221200 <amalmurikkoli2000@gmail.com>
2024-07-13 12:05:45 +0530

    kinde authentication

--- src/app/auth/_components/AuthButtons.tsx
+++ src/app/auth/_components/AuthButtons.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/ui/button'
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import React from 'react'
 
 const AuthButtons = () => {
   return (
     <div className='relative z-50 flex flex-1 flex-col gap-3 md:flex-row'>
-        <Button className='w-full' variant={'outline'}>
-            Sign Up
-        </Button>
-        <Button className='w-full'>
-            Login
-        </Button>
+      <Button className='w-full' variant={'outline'} asChild>
+        <RegisterLink>
+          Sign Up
+        </RegisterLink>
+      </Button>
+      <Button className='w-full' asChild>
+        <LoginLink>
+          Login
+        </LoginLink>
+      </Button>
     </div>
   )
 }
